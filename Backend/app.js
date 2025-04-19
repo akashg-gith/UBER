e81efce9c20318084558ac88b590b8f1f53e8573
Akash Gupta <mr.akashgupta18@gmail.com>
2025-04-20 04:31:07 +0530

    18min

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,10 @@ dotenv.config();
 const cors = require("cors");
 
 const express = require("express");
+const connectToDb = require("./db/db");
 const app = express();
+
+connectToDb();
 
 app.use(cors());
 
