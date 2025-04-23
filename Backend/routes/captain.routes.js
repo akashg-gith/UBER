56aac76f24bfad02232edb29dcd59f3d47a28729
Akash Gupta <mr.akashgupta18@gmail.com>
2025-04-24 03:56:47 +0530

    1hr42min

--- Backend/routes/captain.routes.js
+++ Backend/routes/captain.routes.js
@@ -1,4 +1,5 @@
 const captainController = require("../controllers/captain.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
 const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
@@ -29,5 +30,28 @@ router.post(
       .withMessage("Vehicle Type must be car, motocycle or auto"),
   ],
   captainController.registerCaptain
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be 6 characters long"),
+  ],
+  captainController.loginCaptain
+);
+
+router.get(
+  "/profile",
+  authMiddleware.authCaptain,
+  captainController.getCaptainProfile
+);
+
+router.get(
+  "/logout",
+  authMiddleware.authCaptain,
+  captainController.logoutCaptain
 );
 module.exports = router;
