d75cb13ff74cc5f0c2f17998f66fca41900b70d9
Akash Gupta <mr.akashgupta18@gmail.com>
2025-04-24 03:33:22 +0530

    captain authentication route complete

---
+++ Backend/routes/captain.routes.js
@@ -0,0 +1,33 @@
+const captainController = require("../controllers/captain.controller");
+const express = require("express");
+const router = express.Router();
+const { body } = require("express-validator");
+
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstname")
+      .isLength({ min: 3 })
+      .withMessage("First Name must be 3 characters long"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must be 6 characters long"),
+    body("vehicle.color")
+      .isLength({ min: 3 })
+      .withMessage("Color must be 3 characters long"),
+    body("vehicle.plate")
+      .isLength({ min: 3 })
+      .withMessage("Plate must be 3 characters long"),
+    body("vehicle.capacity")
+      .isNumeric()
+      .withMessage("Capacity must be a number")
+      .isLength({ min: 1 })
+      .withMessage("Capacity must be at least 1"),
+    body("vehicle.vehicleType")
+      .isIn(["car", "motocycle", "auto"])
+      .withMessage("Vehicle Type must be car, motocycle or auto"),
+  ],
+  captainController.registerCaptain
+);
+module.exports = router;
